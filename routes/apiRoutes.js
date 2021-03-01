a628dba5c17834f6da467951d72c62219832454d
alex <alexreveles@yahooo.com>
2021-02-28 22:30:24 -0800

    delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -30,10 +30,9 @@ router.post('/notes', (req, res) => {
 
 
 /////////////////////////// Delete Function for notes  /////////////////////////////
-// router.delete('/notes', (req, res) => {
-
+// router.delete('/notes/:id', (req, res) => {
+    
 // });
 /////////////////////////// Exporting files /////////////////////////////
 module.exports = router;
-
 
